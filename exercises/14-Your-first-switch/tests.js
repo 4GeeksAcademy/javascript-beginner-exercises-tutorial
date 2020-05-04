e688f8ac5270169fd6a765212c03c59532567976
Alejandro Sanchez <aalejo@gmail.com>
2020-05-04 18:12:02 +0000

    fixed

--- exercises/14-Your-first-switch/tests.js
+++ exercises/14-Your-first-switch/tests.js
@@ -1,30 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const rewire = require('rewire');
 
-jest.dontMock('fs');
-//here we are going to store and accumulate (concatenate) all the console log's from the exercise
-let _buffer = "";
-let _log = console.log;
+const file = rewire("./app.js");
+const getColor = file.__get__('getColor');
+test('function getColor should exist', function () {
+    expect(getColor).toBeTruthy();
+});
 
-// lets override the console.log function to mock it,
-// but we are also going to save what supposed to be the ouput of the console inside _buffer
-global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
+test(`When calling getColor('red') it should return true`, function () {
+    expect(getColor('red')).toBe(true);
+});
 
-describe('All the javascript should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-    });
-    afterEach(() => { jest.resetModules(); });
+test(`When calling getColor('green') it should return true`, function () {
+    expect(getColor('green')).toBe(true);
+});
 
-    it('console.log() function should be called with Hello World', function () {
+test(`When calling getColor('blue') it should return true`, function () {
+    expect(getColor('blue')).toBe(true);
+});
 
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./app.js");
-
-        //Expect the console log to have been called with "Hello World" at least one
-        expect(console.log).toHaveBeenCalledWith("Hello World");
-        //and I expect the console.log to be already called just one time.
-        expect(console.log.mock.calls.length).toBe(1);
-
-    });
+test(`When calling getColor('pink') it should return false`, function () {
+    expect(getColor('pink')).toBe(false);
 });
--- exercises/15-Random-numbers/tests.js
+++ exercises/15-Random-numbers/tests.js
@@ -1,1 +1,59 @@
-/* No test built - with consideration that the exercise being almost identical to 11 and may change */
+
+const fs = require('fs');
+const path = require('path');
+const rewire = require('rewire');
+
+jest.dontMock('fs');
+//here we are going to store and accumulate (concatenate) all the console log's from the exercise
+let _buffer = "";
+let _log = console.log;
+
+// lets override the console.log function to mock it,
+// but we are also going to save what supposed to be the ouput of the console inside _buffer
+global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
+
+
+test('function getRandomInt should exist', function () {
+
+    //then I import the index.js (which should have the alert() call inside)
+    const file = rewire("./app.js");
+    const getRandomInt = file.__get__('getRandomInt');
+    
+    //Expect the console log to have been called with a random 0-9 number at least once
+    expect(getRandomInt).toBeTruthy();
+
+});
+
+test('getRandomInt should return something', function () {
+
+    //then I import the index.js (which should have the alert() call inside)
+    const file = rewire("./app.js");
+    const getRandomInt = file.__get__('getRandomInt');
+    
+    //Expect the console log to have been called with a random 0-9 number at least once
+    expect(getRandomInt()).toBeTruthy();
+
+});
+
+test('getRandomInt should return an interger (no decimals)', function () {
+
+    //then I import the index.js (which should have the alert() call inside)
+    const file = rewire("./app.js");
+    const getRandomInt = file.__get__('getRandomInt');
+    
+    expect(Math.floor(_int)).toBe(_int);
+});
+
+test('getRandomInt should return random between 1,10', function () {
+
+    //then I import the index.js (which should have the alert() call inside)
+    const file = rewire("./app.js");
+    const getRandomInt = file.__get__('getRandomInt');
+    
+    //Expect the console log to have been called with a random 0-9 number at least once
+    [0,1,2,3,4,5,6,7,8,9].forEach(() => {
+        const _int = getRandomInt();
+        expect(_int).toBeGreaterThanOrEqual(1);
+        expect(_int).toBeLessThanOrEqual(10);
+    })
+});
