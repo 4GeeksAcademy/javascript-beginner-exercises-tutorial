aeaca8f03ee95a2e3b47bfd89fa00e70a2cc8d8b
Alejandro Sanchez <aalejo@gmail.com>
2021-10-15 17:40:15 +0000

    fixed test fpr 24 bottles

--- exercises/24-Bottles-of-milk/tests.js
+++ exercises/24-Bottles-of-milk/tests.js
@@ -13,24 +13,24 @@ let _log = console.log;
 global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
 
 describe('All the javascript should match', function () {
-    it('console.log() function should be called 99 times', function () {
+    it('console.log() function should be called 99 or 100 times (depending on your approach)', function () {
         const file = require("./app.js");
-        expect(console.log.mock.calls.length).toBe(100);
+        expect([100,99].includes(console.log.mock.calls.length)).toBeTruthy();
     });
     
     it('console.log() function should be called with proper lyrics for more than one bottle', function () {
         const file = require("./app.js");
-        expect(console.log()).toContain("98 bottles of milk on the wall, 98 bottles of milk. Take one down and pass it around, 97 bottles of milk on the wall.");
+        expect(_buffer).toContain("98 bottles of milk on the wall, 98 bottles of milk. Take one down and pass it around, 97 bottles of milk on the wall.");
     });
 
     it('console.log() function should be called with proper lyrics for one bottle', function () {
         const file = require("./app.js");
-        expect(console.log()).toContain("1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.");
+        expect(_buffer).toContain("1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.");
     });
 
     it('console.log() function should be called with proper lyrics for 0 bottles', function () {
         const file = require("./app.js");
-        expect(console.log()).toContain("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.");
+        expect(_buffer).toContain("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.");
     });
     
 });
