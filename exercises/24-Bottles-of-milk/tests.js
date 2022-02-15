f717cb237595375b8000fcf63d761e42b30bd0a5
ricardoyepezz <86753527+ricardoyepezz@users.noreply.github.com>
2022-02-15 16:08:54 -0300

    Update tests.js
    The test asks that at the end of the song the lyrics be changed because it is just a bottle (singular instead of plural). It is necessary to change i >= 1 to i>1 so that the condition is fulfilled.

--- exercises/24-Bottles-of-milk/tests.js
+++ exercises/24-Bottles-of-milk/tests.js
@@ -19,7 +19,7 @@ describe('All the javascript should match', function () {
     
     it('console.log() function should be called with proper lyrics for more than one bottle', function () {
         const file = require("./app.js");
-        for(let i = 99; i >= 1; i--){
+        for(let i = 99; i > 1; i--){
             expect(_buffer).toContain(`${i} bottles of milk on the wall, ${i} bottles of milk. Take one down and pass it around, ${i-1} bottles of milk on the wall.`);
         }
     });
