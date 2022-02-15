c150caa77764a2553fa8c48e248cbda4e91097fc
Martín Suárez <ms.prgramador@gmail.com>
2022-02-15 18:04:51 +0000

    22-Russian-Roulette

--- exercises/22-Russian-Roulette/tests.js
+++ exercises/22-Russian-Roulette/tests.js
@@ -25,14 +25,14 @@ describe('All the javascript should match', function () {
     });
 
     it('If fireGun() is false, message should be "Keep playing :)"', function () {
-            // for (var i = 1; i <= 6; i++) {
-            //     if (i === bulletPosition) {
-            //         expect(fireGun(i)).toContain("You're dead!");
-            //     }
-            //     else {
-            //         expect(fireGun(i)).toContain("Keep playing!");
-            //     }
-            // }
-        expect(fireGun(bulletPosition)).toBe("You're dead!") && expect(fireGun(!bulletPosition)).toBe("Keep Playing!")
+            for (var i = 1; i <= 6; i++) {
+                if (i === bulletPosition) {
+                    expect(fireGun(i)).toContain("You're dead!");
+                }
+                else {
+                    expect(fireGun(i)).toContain("Keep playing!");
+                }
+            }
+        // expect(fireGun(bulletPosition)).toBe("You're dead!") && expect(fireGun(!bulletPosition)).toBe("Keep Playing!")
     });
 });
