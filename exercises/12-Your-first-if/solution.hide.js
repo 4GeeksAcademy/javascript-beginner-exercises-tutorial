3f3f840f97324f547d2ed17504e40ff51a90a969
Martín Suárez <ms.prgramador@gmail.com>
2022-02-17 21:15:25 +0000

    12-Your-first-if working

---
+++ exercises/12-Your-first-if/solution.hide.js
@@ -0,0 +1,7 @@
+if (total <= 50) {
+    console.log("I'm parking. I'll see you right now");
+} else if (total <= 100) {
+    console.log("We'll be there in 5 minutes");
+} else {
+    console.log("We still have a bit of driving left to go");
+}
