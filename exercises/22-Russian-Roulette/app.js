0129369f1e12c503708042515a021c5f9ceb1d56
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-02-21 16:32:20 -0400

    Update app.js

--- exercises/22-Russian-Roulette/app.js
+++ exercises/22-Russian-Roulette/app.js
@@ -1,4 +1,6 @@
-let firePosition = 0; // firePosition will be the position in which the gun will fire.
+// firePosition will be the position in which the gun will fire.
+let firePosition = 1;
+
 // The output of spinChamber will be a number and it can be passed as a parameter to the fireGun function.
 const spinChamber = () => {
 	let chamberPosition = Math.floor((Math.random() * 6) + 1);
