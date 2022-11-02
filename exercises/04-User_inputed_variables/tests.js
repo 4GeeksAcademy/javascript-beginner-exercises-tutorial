77f72ec5d45ba568ab2cd2bdd9dcf5ee8e9127ac
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-11-02 09:21:36 -0400

    Update tests.js

--- exercises/04-User_inputed_variables/tests.js
+++ exercises/04-User_inputed_variables/tests.js
@@ -16,7 +16,7 @@ const app = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8');
 global.console.log = console.log = jest.fn(text => null);
 
 it('Declare age variable', function () {
-    const regex = /let\s*age\s*=\s*prompt\s*\(.*\)/gm
+    const regex = /let\s+age\s*=\s*prompt\s*\(.*\)/gm
     expect(regex.test(app.toString())).toBeTruthy();
 });
 
