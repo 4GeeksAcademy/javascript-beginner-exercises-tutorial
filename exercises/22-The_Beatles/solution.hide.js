fcf886514bc7fe2c4742f94b8ce9c115d1817bc6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-05-24 23:04:55 +0200

    Update solution.hide.js

--- exercises/22-The_Beatles/solution.hide.js
+++ exercises/22-The_Beatles/solution.hide.js
@@ -1,8 +1,8 @@
 function sing(){
     let str = "";
     for(let i = 0; i < 11; i++){
-        if(i === 4) str += 'whisper words of wisdom, ';
-        else if (i === 10) str += 'there will be an answer, let it be';
+        if(i === 4) str += 'there will be an answer, ';
+        else if (i === 10) str += 'whisper words of wisdom, let it be';
         else str += 'let it be, ';
     }
     return str;
