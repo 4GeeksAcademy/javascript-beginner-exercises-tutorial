924546de69812d4921098be9c2bd1fdc33900306
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-05-31 22:47:41 +0200

    Update solution.hide.js

--- exercises/10-Create_a_new_function/solution.hide.js
+++ exercises/10-Create_a_new_function/solution.hide.js
@@ -1,7 +1,7 @@
-function littleIntroduction(name, profession, age) {
+function shortIntroduction(name, profession, age) {
     // Complete this function's body and arguments
     console.log("Hello! my name is " + name + ", my profession is " + profession + ". I am " + age + " years old.")
 }
 
 // Fill the gaps with your data in the correct order
-littleIntroduction("Carlos", "Developer", "35")
+shortIntroduction("Carlos", "Developer", "35")
