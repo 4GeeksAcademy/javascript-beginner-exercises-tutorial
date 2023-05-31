7a03b6065be6cca01471a8cc6d7f43a4eb9daeae
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-05-31 20:43:45 +0200

    new exercise

--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,1 +1,6 @@
-// Add your code here
+function littleIntroduction() {
+  // Complete this function's body and arguments
+}
+
+// Call this function with any data you want!
+littleIntroduction(" ", " ", " ")
