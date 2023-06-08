d33df7b30a8413179fbfa1d6a618e8757dfd2dae
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-06-08 18:48:35 +0000

    updated solution to follow new instructions

--- exercises/10-Create_a_new_function/solution.hide.js
+++ exercises/10-Create_a_new_function/solution.hide.js
@@ -1,7 +1,7 @@
 function shortIntroduction(name, profession, age) {
     // Complete this function's body and arguments
-    console.log("Hello! my name is " + name + ", my profession is " + profession + ". I am " + age + " years old.")
+    return "Hello! my name is " + name + ", my profession is " + profession + ". I am " + age + " years old."
 }
-
+  
 // Fill the gaps with your data in the correct order
-shortIntroduction("Carlos", "Developer", "35")
+console.log(shortIntroduction("Trinity", "Developer", "35"))
